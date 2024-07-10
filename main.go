e2437063c9b9a456346242829140306b9da76018
S. San <38522+kenno@users.noreply.github.com>
2024-07-10 22:59:43 +1000

    clean up tabs and spaces

--- main.go
+++ main.go
@@ -10,75 +10,75 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-        conf := config.New(ctx, "")
-        vpcId := conf.Require("vpcId")
-        subnetId := conf.Require("subnetId")
-        securityGroupId := conf.Require("securityGroupId")
+		conf := config.New(ctx, "")
+		vpcId := conf.Require("vpcId")
+		subnetId := conf.Require("subnetId")
+		securityGroupId := conf.Require("securityGroupId")
 
-        awsConfig := config.New(ctx, "aws")
-        awsRegion := awsConfig.Require("region")
+		awsConfig := config.New(ctx, "aws")
+		awsRegion := awsConfig.Require("region")
 
 		// Create a VPC endpoint for ssm
-        ssmVpcEndpoint, err := ec2.NewVpcEndpoint(ctx, "ssm", &ec2.VpcEndpointArgs{
-            VpcId: pulumi.String(vpcId),
-            ServiceName: pulumi.String(fmt.Sprintf("com.amazonaws.%v.ssm", awsRegion)),
-            VpcEndpointType: pulumi.String("Interface"),
-            SecurityGroupIds: pulumi.StringArray{
-                pulumi.String(securityGroupId),
-            },
-            SubnetIds: pulumi.StringArray{
-                pulumi.String(subnetId),
-            },
-            PrivateDnsEnabled: pulumi.Bool(true),
-            Tags: pulumi.StringMap{
-                "Environment": pulumi.String("test"),
-                "Created": pulumi.String("pulumi"),
-            },
-        })
+		ssmVpcEndpoint, err := ec2.NewVpcEndpoint(ctx, "ssm", &ec2.VpcEndpointArgs{
+			VpcId:           pulumi.String(vpcId),
+			ServiceName:     pulumi.String(fmt.Sprintf("com.amazonaws.%v.ssm", awsRegion)),
+			VpcEndpointType: pulumi.String("Interface"),
+			SecurityGroupIds: pulumi.StringArray{
+				pulumi.String(securityGroupId),
+			},
+			SubnetIds: pulumi.StringArray{
+				pulumi.String(subnetId),
+			},
+			PrivateDnsEnabled: pulumi.Bool(true),
+			Tags: pulumi.StringMap{
+				"Environment": pulumi.String("test"),
+				"Created":     pulumi.String("pulumi"),
+			},
+		})
 
 		if err != nil {
 			return err
 		}
 
 		// Create a VPC endpoint for ssm
-        ec2messagesVpcEndpoint, err := ec2.NewVpcEndpoint(ctx, "ec2messages", &ec2.VpcEndpointArgs{
-            VpcId: pulumi.String(vpcId),
-            ServiceName: pulumi.String(fmt.Sprintf("com.amazonaws.%v.ec2messages", awsRegion)),
-            VpcEndpointType: pulumi.String("Interface"),
-            SecurityGroupIds: pulumi.StringArray{
-                pulumi.String(securityGroupId),  
-            },
-            SubnetIds: pulumi.StringArray{
-                pulumi.String(subnetId),
-            },
-            PrivateDnsEnabled: pulumi.Bool(true),
-            Tags: pulumi.StringMap{
-                "Environment": pulumi.String("test"),
-                "Created": pulumi.String("pulumi"),
-            },
-        })
+		ec2messagesVpcEndpoint, err := ec2.NewVpcEndpoint(ctx, "ec2messages", &ec2.VpcEndpointArgs{
+			VpcId:           pulumi.String(vpcId),
+			ServiceName:     pulumi.String(fmt.Sprintf("com.amazonaws.%v.ec2messages", awsRegion)),
+			VpcEndpointType: pulumi.String("Interface"),
+			SecurityGroupIds: pulumi.StringArray{
+				pulumi.String(securityGroupId),
+			},
+			SubnetIds: pulumi.StringArray{
+				pulumi.String(subnetId),
+			},
+			PrivateDnsEnabled: pulumi.Bool(true),
+			Tags: pulumi.StringMap{
+				"Environment": pulumi.String("test"),
+				"Created":     pulumi.String("pulumi"),
+			},
+		})
 
 		if err != nil {
 			return err
 		}
 
 		// Create a VPC endpoint for ssm
-        ssmmessagesVpcEndpoint, err := ec2.NewVpcEndpoint(ctx, "ssmmessages", &ec2.VpcEndpointArgs{
-            VpcId: pulumi.String(vpcId),
-            ServiceName: pulumi.String(fmt.Sprintf("com.amazonaws.%v.ssmmessages", awsRegion)),
-            VpcEndpointType: pulumi.String("Interface"),
-            SecurityGroupIds: pulumi.StringArray{
-                pulumi.String(securityGroupId),
-            },
-            SubnetIds: pulumi.StringArray{
-                pulumi.String(subnetId),
-            },
-            PrivateDnsEnabled: pulumi.Bool(true),
-            Tags: pulumi.StringMap{
-                "Environment": pulumi.String("test"),
-                "Created": pulumi.String("pulumi"),
-            },
-        })
+		ssmmessagesVpcEndpoint, err := ec2.NewVpcEndpoint(ctx, "ssmmessages", &ec2.VpcEndpointArgs{
+			VpcId:           pulumi.String(vpcId),
+			ServiceName:     pulumi.String(fmt.Sprintf("com.amazonaws.%v.ssmmessages", awsRegion)),
+			VpcEndpointType: pulumi.String("Interface"),
+			SecurityGroupIds: pulumi.StringArray{
+				pulumi.String(securityGroupId),
+			},
+			SubnetIds: pulumi.StringArray{
+				pulumi.String(subnetId),
+			},
+			PrivateDnsEnabled: pulumi.Bool(true),
+			Tags: pulumi.StringMap{
+				"Environment": pulumi.String("test"),
+				"Created":     pulumi.String("pulumi"),
+			},
+		})
 
 		if err != nil {
 			return err
